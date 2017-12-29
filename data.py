6bb89fdbd17747b5ee62f2fbe2f3bca84f2ee9f2
lum <lum@localhost.localdomain>
2017-12-29 09:23:48 +0800

    remove glob

--- data.py
+++ data.py
@@ -154,14 +154,13 @@ class dataProcess(object):
 		print('-'*30)
 		print('Creating training images...')
 		print('-'*30)
-		imgs = glob.glob(self.data_path+"/*."+self.img_type)
+                imgs = os.listdir(self.data_path)
 		print(len(imgs))
 		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
 		imglabels = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
 		for imgname in imgs:
-			midname = imgname[imgname.rindex("/")+1:]
-			img = load_img(self.data_path + "/" + midname,grayscale = True)
-			label = load_img(self.label_path + "/" + midname,grayscale = True)
+			img = load_img(self.data_path + "/" + imgname,grayscale = True)
+			label = load_img(self.label_path + "/" + imgname,grayscale = True)
 			img = img_to_array(img)
 			label = img_to_array(label)
 			#img = cv2.imread(self.data_path + "/" + midname,cv2.IMREAD_GRAYSCALE)
@@ -183,12 +182,13 @@ class dataProcess(object):
 		print('-'*30)
 		print('Creating test images...')
 		print('-'*30)
-		imgs = glob.glob(self.test_path+"/*."+self.img_type)
+#		imgs = glob.glob(self.test_path+"/*."+self.img_type)
+                imgs = os.listdir(self.test_path)
 		print(len(imgs))
 		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
 		for imgname in imgs:
-			midname = imgname[imgname.rindex("/")+1:]
-			img = load_img(self.test_path + "/" + midname,grayscale = True)
+#			midname = imgname[imgname.rindex("/")+1:]
+			img = load_img(self.test_path + "/" + imgname,grayscale = True)
 			img = img_to_array(img)
 			#img = cv2.imread(self.test_path + "/" + midname,cv2.IMREAD_GRAYSCALE)
 			#img = np.array([img])
